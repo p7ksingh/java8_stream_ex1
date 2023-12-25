decb2bf22758760c1a1807029fcc50237379db33
Pankaj Singh <121671526+pankajkscs@users.noreply.github.com>
2023-12-25 23:06:07 +0530

    limit()

---
+++ StreamClient5.java
@@ -0,0 +1,22 @@
+package com.java8.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamClient5 {
+    public static void main(String[] args) {
+        List<String> lines = new ArrayList<>();
+        lines.add("us");
+        lines.add("india");
+        lines.add("uk");
+        lines.add("us");
+        lines.add(null);
+        lines.add("india");
+        lines.add("india");
+        lines.add("japan");
+        // remove dublicate element and null elements and short data from arraylist
+        lines.stream().filter(dt -> dt != null).distinct().sorted().map(dt -> dt.toUpperCase()).limit(2)
+                .forEach(dt -> System.out.println(dt));
+
+    }
+}
